2af207f202a5e2fb7ec0615df637715639f0891c
pranshusaini03 <pranshusaini03@gmail.com>
2025-01-03 12:18:13 +0530

    first commit

---
+++ server.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+
+# Load the trained model
+with open(r'C:\Users\Pranshu Saini\Desktop\New folder\model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+# Load the saved scaler
+
+app = Flask(__name__)
+
+# Route for the main HTML page
+@app.route('/')
+def home():
+    return render_template('abc.html')
+
+# Route to handle form submission
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get form data
+    ig = float(request.form['ig'])
+    cgpa = float(request.form['cgpa'])
+
+    # Prepare data for prediction
+    input_data = np.array([[cgpa, ig]])
+
+    # Scale input data using the pre-fitted scaler
+    #input_data_scaled = scaler.transform(input_data)
+
+    # Predict using the model
+    prediction = model.predict(input_data)
+
+    # Convert prediction to readable text
+    placement_status = 'Placed' if prediction == 1 else 'Not Placed'
+    
+    # Return the result as a JSON object
+    return jsonify({'placement': placement_status})
+
+if __name__ == '__main__':
+    app.run(debug=True)
